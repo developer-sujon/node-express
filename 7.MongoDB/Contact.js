9b794b9426137e9be22352c52c5f89658fd7d883
developer-sujon <mohammad.sujon@outlook.com>
2022-02-25 06:36:09 +0600

    MongoDB Rest Api

---
+++ 7.MongoDB/Contact.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require("mongoose");
+
+const contactSchema = new Schema({
+  name: String,
+  phone: Number,
+  email: String,
+});
+
+const Contact = model("contact", contactSchema);
+
+module.exports = Contact;
